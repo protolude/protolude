05c32a4542aa66f715aea3855e140f74e97d4852
Stephen Diehl <stephen.m.diehl@gmail.com>
2016-04-13 12:46:00 -0400

    adds compiler-independent base module

---
+++ src/Base.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE Unsafe #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module Base (
+  module X,
+  ($!),
+) where
+
+-- Glorious Glashgow Haskell Compiler
+#if defined(__GLASGOW_HASKELL__) && ( __GLASGOW_HASKELL__ >= 600 )
+
+-- Base GHC types
+import GHC.Num as X
+import GHC.Enum as X
+import GHC.Real as X
+import GHC.Float as X
+import GHC.Show as X
+import GHC.Types as X
+import GHC.Exts as X (
+    Constraint
+  , Ptr
+  , FunPtr
+  , the
+  )
+import GHC.Base as X (
+    (++)
+  , seq
+  , asTypeOf
+  )
+import System.IO as X (
+    print
+  , putStr
+  , putStrLn
+  )
+
+infixr 0 $!
+
+($!) :: (a -> b) -> a -> b
+f $! x  = let !vx = x in f vx
+
+#endif
+
+-- Simple Haskell Compiler
+#if defined(__SHC_HASKELL__)
+
+import SHC.Prim as X
+import SHC.Types as X
+import SHC.Classes as X
+
+#endif
