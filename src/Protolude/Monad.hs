f6fa0460a92574db013a6432926652819a88b90a
sdiehl <stephen.m.diehl@gmail.com>
2020-01-04 19:37:46 +0000

    Use liftM because of pre-AMP quirkk

--- src/Protolude/Monad.hs
+++ src/Protolude/Monad.hs
@@ -45,7 +45,7 @@ import Data.List (concat)
 import Control.Monad
 
 concatMapM :: Monad m => (a -> m [b]) -> [a] -> m [b]
-concatMapM f xs = fmap concat (mapM f xs)
+concatMapM f xs = liftM concat (mapM f xs)
 
 liftM' :: Monad m => (a -> b) -> m a -> m b
 liftM' = (<$!>)
