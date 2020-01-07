3ed0789d0029d6206ae9984a6d4cb44969bebf88
sdiehl <stephen.m.diehl@gmail.com>
2020-01-07 11:47:54 +0000

    Update nix derivation

--- default.nix
+++ default.nix
@@ -1,5 +1,16 @@
-{ pkgs ? import <nixpkgs> {}
+{ mkDerivation, array, async, base, bytestring, containers, deepseq
+, ghc-prim, hashable, mtl, mtl-compat, stdenv, stm, text
+, transformers, transformers-compat
 }:
-pkgs.haskellPackages.developPackage {
-  root = ./.;
+mkDerivation {
+  pname = "protolude";
+  version = "0.3.0";
+  src = ./protolude;
+  libraryHaskellDepends = [
+    array async base bytestring containers deepseq ghc-prim hashable
+    mtl mtl-compat stm text transformers transformers-compat
+  ];
+  homepage = "https://github.com/sdiehl/protolude";
+  description = "A small prelude";
+  license = stdenv.lib.licenses.mit;
 }
