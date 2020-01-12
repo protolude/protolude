06d255c477e819c3a6fe2fbcc07edc3c2a904d7c
sdiehl <stephen.m.diehl@gmail.com>
2020-01-12 17:01:31 +0000

    Generate export lists for #112

---
+++ Exports.hs
@@ -0,0 +1,55 @@
+module Main
+  ( main,
+  )
+where
+
+import Control.Monad.Trans
+import qualified Data.List as List
+import Data.Maybe
+import DynFlags
+import GHC
+import GHC.Paths
+import Outputable
+import System.FilePath.Posix
+import Prelude hiding (mod)
+
+autoModule :: FilePath -> IO ()
+autoModule mod = runGhc (Just GHC.Paths.libdir) $ do
+  dflags <- GHC.getSessionDynFlags
+  _ <-
+    setSessionDynFlags
+      ( dflags `gopt_set` Opt_KeepRawTokenStream
+          `gopt_set` Opt_Haddock
+          `gopt_set` Opt_GhciSandbox
+      )
+  target <- guessTarget ("src" </> addExtension mod ".hs") Nothing
+  setTargets [target]
+  _ <- load LoadAllTargets
+  modSum <- getModSummary $ mkModuleName mod
+  p <- GHC.parseModule modSum
+  t <- typecheckModule p
+  let modInfo = tm_checked_module_info t
+  let exports = modInfoExports modInfo
+  exportThings <- sequence <$> mapM lookupName exports
+  let sortedThings = List.sortOn getOccName (concat exportThings)
+  liftIO $ mapM_ (showThing) sortedThings
+  pure ()
+
+showNamed :: NamedThing a => a -> IO ()
+showNamed a = do
+  let nm = showGhc (getOccName a)
+  let mod = showGhc (nameModule (getName a))
+  putStrLn (nm ++ " from " ++ mod)
+  pure ()
+
+showThing :: TyThing -> IO ()
+showThing (AnId a) = showNamed a
+showThing (AConLike a) = showNamed a
+showThing (ATyCon a) = showNamed a
+showThing _ = error "Should never happen."
+
+showGhc :: (Outputable a) => a -> String
+showGhc = showPpr unsafeGlobalDynFlags
+
+main :: IO ()
+main = autoModule "Protolude"
